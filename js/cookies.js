695fe07e56c403ab861690c0be63fc5d9c611036
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-03-04 00:09:11 -0800

    update displayCookie to have a null check
    updated deleteCookie to clear table also

--- js/cookies.js
+++ js/cookies.js
@@ -1,4 +1,9 @@
+/**
+ * Cookies is a class that will hold all the function needed for the Cookie app.
+ * @type {Object}
+ */
 var cookies = new Object({
+  // function to set the cookie with a 1 day expiration
   setCookie: function setCookie (name, age, word) {
     var today = new Date();
     var oneDayExpiry = new Date(today.getTime() + 1 * 24 * 3600 * 1000);
@@ -10,13 +15,24 @@ var cookies = new Object({
     });
     document.cookie = "demoCookie=" + cookie + ";" + cExpire;
   },
+  // function to output the cookie data to our table on _table.html
   displayCookie: function displayCookie () {
-    var data = JSON.parse(document.cookie.split('=')[1]);
-    document.getElementById('ans1').innerHTML = data.name;
-    document.getElementById('ans2').innerHTML = data.age;
-    document.getElementById('ans3').innerHTML = data.word;
+    var cookie = document.cookie.split('=')[1];
+    console.log(cookie);
+    if (cookie == null){
+      alert('No more cookies :(');
+    } else {
+      var data = JSON.parse(cookie);
+      document.getElementById('ans1').innerHTML = data.name;
+      document.getElementById('ans2').innerHTML = data.age;
+      document.getElementById('ans3').innerHTML = data.word;
+    }
   },
+  // function to delete the cookie
   deleteCookie: function deleteCookie () {
-    document.cookie = "name=;expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    document.cookie = "demoCookie=;expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    document.getElementById('ans1').innerHTML = "";
+    document.getElementById('ans2').innerHTML = "";
+    document.getElementById('ans3').innerHTML = "";
   }
 });
