0376da6e539f95dfbf11d4d8830488833cf3525b
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-03-03 23:43:15 -0800

    complete functional js
    deletecookie, setcookie, displaycookie

--- js/cookie.js
+++ js/cookie.js
@@ -1,15 +1,44 @@
-$( document ).ready(function() {
+$( document ).ready( function () {
 
-  function setCookie (name, age) {
-    var count =
+  function setCookie (name, age, word) {
     var today = new Date();
-    var day1 = new Date(today.getTime() + 1 * 24 * 3600 * 1000);
-    document.cookie = "name="+name + ";" + "age="+age + "; expires=" + day1.toGMTString();
+    var oneDayExpiry = new Date(today.getTime() + 1 * 24 * 3600 * 1000);
+    var cExpire = "expires=" + oneDayExpiry.toGMTString();
+    var cookie = JSON.stringify({
+      name: name,
+      age: age,
+      word: word
+    });
+    document.cookie = "demoCookie=" + cookie + ";" + cExpire;
   }
 
-  $(document).on("click", ".btn", function() {
+  function deleteCookie () {
+    document.cookie = "name=;expires=Thu, 01 Jan 1970 00:00:00 UTC";
+  }
+
+  function displayCookie () {
+    var data = JSON.parse(document.cookie.split('=')[1]);
+    document.getElementById('ans1').innerHTML = data.name;
+    document.getElementById('ans2').innerHTML = data.age;
+    document.getElementById('ans3').innerHTML = data.word;
+  }
+
+  $(document).on("click", ".save", function() {
     // set cookies
     // update cookie table
+    var name = document.getElementById('name').value;
+    var age = document.getElementById('age').value;
+    var word = document.getElementById('word').value;
+    setCookie(name,age,word);
+    displayCookie();
+    $(".display").hide();
+  });
+
+  $(document).on("click", ".display", function() {
+    // output current cookie
+    displayCookie();
+    // hide the button
+    $(this).hide();
   });
 
 });
