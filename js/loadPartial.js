591d2d08afc0387377c79834a8fa8ea849579548
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-03-04 11:21:15 -0800

    load the partial _javascript.html

--- js/loadPartial.js
+++ js/loadPartial.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
     $('.form-container').load('_form.html');
     $('.table-container').load('_table.html');
+    $('.javascript-container').load('_javascript.html');
 });
